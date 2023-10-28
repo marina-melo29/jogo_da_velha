466f9b6c9c91bb642c4a833b42f9df59c6e17c7c
marina-melo29 <ninadsm29@gmail.com>
2023-10-28 13:00:16 -0300

    adjusts

--- start.rb
+++ start.rb
@@ -40,36 +40,25 @@ class Start < GameIntro
         puts 'sua vez!'
         puts 'digite um número de 1 a 9'
 
-        player_step = get_step
-        valid_step = valid_step?(player_step, empty_boxes)
-
-        if valid_step == false
-          player_step = request_step_until_valid
-        else
-          player_step -= 1
-        end
-
+        player_step = player_turn
         win = will_step_get_a_win?(1, player_step)
 
         @boxes[player_step] = 1
 
         draw_figure(@boxes)
-
-        @end_game = true if win
+        end_game_check(win)
       else
         # Vez do bot
         puts 'vez do oponente . . .'
-
         sleep(2) # esperar 2 segundos antes de continuar (pra não printar tão rápido, e ficar mais natural)
 
-        bot_step = bot_turn(empty_boxes, bot_steps)
-        win = will_step_get_a_win?(0, bot_step)
+        bot_turn = bot_turn(empty_boxes, bot_steps)
+        win = will_step_get_a_win?(0, bot_turn)
 
-        @boxes[bot_step] = 0
+        @boxes[bot_turn] = 0
 
         draw_figure(@boxes)
-
-        @end_game = true if win
+        end_game_check(win)
 
         bot_steps += 1
       end
@@ -99,6 +88,19 @@ class Start < GameIntro
     possibilities
   end
 
+  def player_turn
+    player_step = get_step
+    valid_step = valid_step?(player_step)
+
+    if valid_step == false
+      player_step = request_step_until_valid
+    else
+      player_step -= 1
+    end
+
+    player_step
+  end
+
   def bot_turn(empty_boxes, bot_steps)
     return empty_boxes.sample if bot_steps.zero? # Se for a primeira jogada do robô, então escolher aleatório.
 
@@ -110,6 +112,22 @@ class Start < GameIntro
     return opponent_win_possibilities.first if opponent_win_possibilities.count > 0
 
     empty_boxes.sample
+  end
+
+  def end_game_check(win)
+    if win
+      @end_game = true
+      final_win_message(check_turn)
+    elsif game_ended_in_draw?
+      @end_game = true
+      draw_message
+    end
+  end
+
+  def game_ended_in_draw?
+    return true if @boxes.all? { |box| box != -1 }
+
+    false
   end
 
   def win_possibilities(empty_boxes, turn)
@@ -137,10 +155,10 @@ class Start < GameIntro
       simulated_boxes[index] = turn
     end
 
-    check_game_situation(turn, simulated_boxes)
+    check_game_win(turn, simulated_boxes)
   end
 
-  def check_game_situation(turn, boxes = @boxes)
+  def check_game_win(turn, boxes = @boxes)
     win = false
 
     WINNING_COMBINATIONS.each do |combo|
@@ -177,8 +195,8 @@ class Start < GameIntro
     valid_step - 1 # -1 pois os cálculos se baseiam nas caixas de 0 a 8, e não 1 a 9.
   end
 
-  def valid_step?(chosen_step, empty_boxes)
-    false if empty_boxes.include?(chosen_step - 1)
+  def valid_step?(chosen_step)
+    return false if possibilities.exclude?(chosen_step - 1)
 
     return true if chosen_step.instance_of?(Integer) && (chosen_step >= 1) && (chosen_step <= 9)
 
@@ -213,5 +231,31 @@ class Start < GameIntro
     return 'x' if turn > 0
 
     'o'
+  end
+
+  def final_win_message(turn)
+    if turn.positive?
+      congratulation_message
+    else
+      lost_message
+    end
+  end
+
+  def congratulation_message
+    puts '=============================='
+    puts '   PARABÉENS!! VOCÊ GANHOU!'
+    puts '=============================='
+  end
+
+  def lost_message
+    puts '=============================='
+    puts '         VOCÊ PERDEU!         '
+    puts '=============================='
+  end
+
+  def draw_message
+    puts '=============================='
+    puts '            EMPATE            '
+    puts '=============================='
   end
 end
